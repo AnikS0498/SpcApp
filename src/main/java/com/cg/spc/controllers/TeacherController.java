0b346ec31dec3dd03a4700301e753cec4b2dbb09
P08Rahul <p08rahul@gmail.com>
2021-04-12 21:07:02 +0530

    rahul commit teacher controller started

--- src/main/java/com/cg/spc/controllers/TeacherController.java
+++ src/main/java/com/cg/spc/controllers/TeacherController.java
@@ -1,12 +1,25 @@
 package com.cg.spc.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cg.spc.entities.Exam;
+import com.cg.spc.services.IExamService;
 
 @RestController
 @RequestMapping("/teacher")
 public class TeacherController {
 	
+	@Autowired
+	private IExamService examService;
+	
+	@PostMapping("/exam/add")
+	public Exam addExamDetails(@RequestBody Exam exam)
+	{
+		return examService.addExam(exam);
+	}
 
 }
