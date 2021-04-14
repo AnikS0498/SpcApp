aec026943e124c170433cb5a307f4f4ef2374c50
AnikS0498 <anikeshsinha041198@gmail.com>
2021-04-14 11:15:55 +0530

    Add exceptions

---
+++ src/main/java/com/cg/spc/exceptions/SPCGlobalExceptionHandler.java
@@ -0,0 +1,46 @@
+package com.cg.spc.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class SPCGlobalExceptionHandler {
+
+	@ExceptionHandler(value = StudentNotFoundException.class)
+	public ResponseEntity<Object> handleStudentNotFoundException(StudentNotFoundException ex){
+		
+		String response = "This student does not exist";
+		return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(value = DiaryNotFoundException.class)
+	public ResponseEntity<Object> handleDiaryNotFoundException(DiaryNotFoundException ex){
+		
+		String response = "This diary does not exist";
+		return new ResponseEntity<Object>(response, HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(value = ExamNotFoundException.class)
+	public ResponseEntity<Object> handleExamNotFoundException(ExamNotFoundException ex){
+		
+		String response = "This exam does not exist";
+		return new ResponseEntity<Object>(response, HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(value = FeeNotFoundException.class)
+	public ResponseEntity<Object> handleFeeNotFoundException(FeeNotFoundException ex){
+		
+		String response = "Fee details not found";
+		return new ResponseEntity<Object>(response, HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(value = ReportCardNotFoundException.class)
+	public ResponseEntity<Object> handleReportCardNotFoundException(ReportCardNotFoundException ex){
+		
+		String response = "Report card not found";
+		return new ResponseEntity<Object>(response, HttpStatus.NOT_FOUND);
+	}
+	
+}
