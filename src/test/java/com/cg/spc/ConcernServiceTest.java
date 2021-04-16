81d0554e9605db3adaa608bc7235427784324eff
P08Rahul <p08rahul@gmail.com>
2021-04-16 16:21:03 +0530

    rahul concernTest

--- src/test/java/com/cg/spc/ConcernServiceTest.java
+++ src/test/java/com/cg/spc/ConcernServiceTest.java
@@ -1,8 +1,10 @@
 package com.cg.spc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,7 +16,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import com.cg.spc.entities.Concern;
 import com.cg.spc.entities.ConcernType;
 import com.cg.spc.entities.Parent;
+import com.cg.spc.exceptions.ParentNotFoundException;
 import com.cg.spc.repositories.IConcernRepository;
+import com.cg.spc.repositories.IParentRepository;
 import com.cg.spc.services.IConcernService;
 
 @SpringBootTest
@@ -25,53 +29,53 @@ public class ConcernServiceTest {
 
 	@MockBean
 	private IConcernRepository concernRepository;
+	
+	@MockBean
+	private IParentRepository parentRepository;
 
 	Concern concern;
 
-	Concern concern2;
-
+	Parent parent;
+	
 	@BeforeEach
 	public void init() {
 		concern = new Concern();
 		concern.setConcern("child getting low marks");
 		concern.setConcernType(ConcernType.ACADEMIC);
-		Parent parent = new Parent();
-		parent.setId(1);
+		parent = new Parent();
+		parent.setId(500);
 		concern.setParent(parent);
-
-		concern2 = new Concern();
-		concern2.setConcernType(ConcernType.FEES);
-		concern2.setConcern("Fee details is not correct");
-		Parent parent2 = new Parent();
-		parent2.setId(2);
-		concern2.setParent(parent2);
 	}
 
 	@Test
 	@DisplayName("positive test case for add concern")
 	public void testAddConcern() {
+		Mockito.when(parentRepository.findById(500)).thenReturn(Optional.of(parent));
 		Mockito.when(concernRepository.save(concern)).thenReturn(concern);
-		assertEquals(concern, concernService.addConcern(concern, 1));
+		assertEquals(concern, concernService.addConcern(concern, 500));
 	}
 
 	@Test
 	@DisplayName("negative test case for add concern")
 	public void testAddConcernNegative() {
-		Mockito.when(concernRepository.save(concern2)).thenReturn(concern2);
-		assertNotEquals(concern2, concernService.addConcern(concern, 1));
+		Mockito.when(parentRepository.findById(500)).thenReturn(Optional.of(parent));
+		Mockito.when(concernRepository.save(concern)).thenReturn(concern);
+		Assertions.assertThrows(ParentNotFoundException.class, ()->concernService.updateConcern(concern, 501));
 	}
 	
 	@Test
-	@DisplayName("positive test case for add concern")
+	@DisplayName("positive test case for update concern")
 	public void testUpdateConcern() {
+		Mockito.when(parentRepository.findById(500)).thenReturn(Optional.of(parent));
 		Mockito.when(concernRepository.save(concern)).thenReturn(concern);
-		assertEquals(concern, concernService.updateConcern(concern, 1));
+		assertEquals(concern, concernService.updateConcern(concern, 500));
 	}
 
 	@Test
-	@DisplayName("negative test case for add concern")
+	@DisplayName("negative test case for update concern")
 	public void testUpdateConcernNegative() {
-		Mockito.when(concernRepository.save(concern2)).thenReturn(concern2);
-		assertNotEquals(concern2, concernService.updateConcern(concern, 1));
+		Mockito.when(parentRepository.findById(500)).thenReturn(Optional.of(parent));
+		Mockito.when(concernRepository.save(concern)).thenReturn(concern);
+		Assertions.assertThrows(ParentNotFoundException.class, ()->concernService.updateConcern(concern, 501));
 	}
 }
